a8b5d2e5c72befc507d07a6379a681f5fa592fbe
Endminis <kovalenkoo562@gmail.com>
2022-06-16 19:58:01 +0300

    Removed second variable in String.cs

--- LAB_1_PROGA/Program.cs
+++ LAB_1_PROGA/Program.cs
@@ -1,99 +1,41 @@
 ﻿using Newtonsoft.Json;
 
-public class String
-{
-    private string str1 = "";
-    private string str2 = "";
 
-    public string Str1
-    {
-    get
-        {
-            return str1;
-        }
-        set
-        {
-            str1 = value;
-        }
-        }   public string Str2
-    {
-    get
-        {
-            return str2;
-        }
-        set
-        {
-            str2 = value;
-        }
-        }
-
-    public string concatenate(string str1, string str2)
-    {
-        return str1 + str2;
-    }
-    public string removeSubstring(string str1, string str2)
-    {
-        if (str1.IndexOf(str2) != -1)
-        {
-            return str1.Remove(str1.IndexOf(str2), str2.Length);
-        } else
-        {
-            return "Другого рядка немає в першому!";
-        }
-    }
-    public string compare(string str1, string str2)
-    {
-        if (str1.Equals(str2))
-        {
-            return "Рядки однакові";
-        } else 
-        {
-              if(str1.Length == str2.Length)
-            {
-                return "Однакова довжина, але рядки не рівні";
-            } else if(str1.Length > str2.Length)
-            {
-                return "Перший рядок більший за другий";
-            } else {
-                return "Перший рядок менший за другий";
-                    }
-        }
-    }
-    static void Main(string[] args)
-    {  
-        
-        String s = new String();
+        String string1 = new String();
+        String string2 = new String();
         Console.WriteLine("Введіть перший рядок");
-        s.str1 = Console.ReadLine();
+        string1.Str = Console.ReadLine();
         Console.WriteLine("Введіть другий рядок");
-         s.str2 = Console.ReadLine();
-        string output = JsonConvert.SerializeObject(s);
+        string2.Str = Console.ReadLine();
+        var output1 = JsonConvert.SerializeObject(string1);
+        var output2 = JsonConvert.SerializeObject(string2);
 
-        String deserializedProduct = JsonConvert.DeserializeObject<String>(output);
+        String deserializedProduct1 = JsonConvert.DeserializeObject<String>(output1);
+        String deserializedProduct2 = JsonConvert.DeserializeObject<String>(output2);
 
-        if (deserializedProduct != null)
+        if (deserializedProduct1 != null && deserializedProduct2 != null)
         {
             Console.WriteLine("Введіть:\n1 - конкатенація двох рядків\n2 - вирізання підрядка\n3 - порівняння двох рядків");
             int input = Convert.ToInt32(Console.ReadLine());
             switch (input)
             {
                 case 1:
-                    Console.WriteLine("Результат:\n" + deserializedProduct.concatenate(deserializedProduct.str1, deserializedProduct.str2));
+                    Console.WriteLine("Результат:\n" + deserializedProduct1.concatenate(deserializedProduct1.Str, deserializedProduct2.Str));
                     break;
                 case 2:
-                    Console.WriteLine("Результат:\n" + deserializedProduct.removeSubstring(deserializedProduct.str1, deserializedProduct.str2));
+                    Console.WriteLine("Результат:\n" + deserializedProduct1.removeSubstring(deserializedProduct1.Str, deserializedProduct2.Str));
                     break;
                 case 3:
-                    Console.WriteLine("Результат:\n" + deserializedProduct.compare(deserializedProduct.str1, deserializedProduct.str2));
+                    Console.WriteLine("Результат:\n" + deserializedProduct1.compare(deserializedProduct1.Str, deserializedProduct2.Str));
                     break;
                 default:
                     Console.WriteLine("Не можу розпізнати метод!");
                     break;
             }
 
-        } else
+        }
+        else
         {
             Console.WriteLine("Неправильно введений рядок!");
         }
-    }
-}
+    
---
+++ LAB_1_PROGA/String.cs
@@ -0,0 +1,54 @@
+﻿    public class String
+    {
+        private string _str = "";
+        public string Str
+        {
+            get
+            {
+                return _str;
+            }
+            set
+            {
+                _str = value;
+            }
+
+        }
+
+        public string concatenate(string str1, string str2)
+        {
+            return str1 + str2;
+        }
+        public string removeSubstring(string str1, string str2)
+        {
+            if (str1.IndexOf(str2) != -1)
+            {
+                return str1.Remove(str1.IndexOf(str2), str2.Length);
+            }
+            else
+            {
+                return "Другого рядка немає в першому!";
+            }
+        }
+        public string compare(string str1, string str2)
+        {
+            if (str1.Equals(str2))
+            {
+                return "Рядки однакові";
+            }
+            else
+            {
+                if (str1.Length == str2.Length)
+                {
+                    return "Однакова довжина, але рядки не рівні";
+                }
+                else if (str1.Length > str2.Length)
+                {
+                    return "Перший рядок більший за другий";
+                }
+                else
+                {
+                    return "Перший рядок менший за другий";
+                }
+            }
+        }
+}
